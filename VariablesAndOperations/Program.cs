215db50add18eba854942fe39e156ce5ef7382f1
rebeccaseel <rseelgws@gmail.com>
2017-06-27 15:04:54 -0400

    in class work

--- VariablesAndOperations/Program.cs
+++ VariablesAndOperations/Program.cs
@@ -10,6 +10,50 @@ namespace VariablesAndOperations
     {
         static void Main(string[] args)
         {
+           
+            int age = 65;
+            string why = "because I said so";
+            char firstInitial = 'L';
+
+            //floating point types
+            float floatNumber = 2.16440334897294849383961032f;
+            Console.WriteLine(floatNumber);
+
+            double doubleNumber = 2.16440334897294849383961032d;
+            Console.WriteLine(doubleNumber);
+
+            decimal decimalNumber = 2.16440334897294849383961032m;
+            Console.WriteLine(decimalNumber);
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge < samAge);
+
+
+            Console.WriteLine("What is the name on the reservation?");
+            string reserveName = Console.ReadLine();
+
+            Console.WriteLine("How many people are in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            double amountEach = 13.53d;
+            double totalBill = numberOfPeople * amountEach;
+            Console.WriteLine(totalBill);
+
+
+            Console.WriteLine("Reservation: " + reserveName);
+            Console.WriteLine("Total Due: $" + totalBill);
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
